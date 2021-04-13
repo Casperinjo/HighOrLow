67353d337fee2c474514946c143515c9e8ab0898
Casperinjo <Casper.karlsson3@elev.ga.ntig.se>
2021-04-13 18:38:03 +0200

    Har gjort klart skapandet av korten och börjat på randomiseringen.

---
+++ HighOrLow/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighOrLow
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            SkapaKortlek[,] kortlek = SkapaKortlek.CreateCards();
+            List<string> speladeKort = new List<string>();
+            Random randNummer = new Random();
+            Random randTyp = new Random();
+            int kortNummer;
+            int kortTyp;
+            string spelatKort;
+
+            for (int i = 0; i < 52; i++)
+            {
+                while (true)
+                {
+                    kortNummer = randNummer.Next(0, 13);
+                    kortTyp = randTyp.Next(0, 4);
+                    spelatKort = kortTyp.ToString() + kortNummer.ToString();
+
+                    if (speladeKort.Contains(spelatKort) == true)
+                    {
+                        Console.WriteLine(spelatKort);
+                        continue;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                kortlek[kortTyp, kortNummer].ShowCards();
+                speladeKort.Add(spelatKort);
+
+                Console.ReadLine();
+
+                Console.WriteLine(i + " = i");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ HighOrLow/SkapaKortlek.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighOrLow
+{
+    public enum kortStorlek { två, tre, fyra, fem, sex, sju, åtta, nio, tio, knekt, dam, kung, ess };
+
+    public enum kortTyp { spader, hjärter, ruter, klöver };
+
+    internal class SkapaKortlek
+    {
+        private kortStorlek kortNummer;
+        private kortTyp kortAttribut;
+
+        public SkapaKortlek(kortStorlek kortNummer, kortTyp kortAttribut)
+        {
+            this.kortNummer = kortNummer;
+            this.kortAttribut = kortAttribut;
+        }
+
+        public void ShowCards()
+        {
+            Console.WriteLine(kortNummer + " " + kortAttribut);
+        }
+
+        static public SkapaKortlek[,] CreateCards()
+        {
+            SkapaKortlek[,] kort = new SkapaKortlek[4, 13];
+            for (int i = 0; i < 13; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    kort[j, i] = new SkapaKortlek((kortStorlek)i, (kortTyp)j);
+                }
+            }
+            return kort;
+        }
+    }
+}
